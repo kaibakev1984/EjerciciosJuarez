20ba4a61ec727a2c54b7b7f390fdbb6c55eaa1c1
Lorblak <vasquez97548@gmail.com>
2020-01-31 23:12:47 -0300

    Se documentaron los métodos en ejercicio_provincias_lista; se agregaron algunos cambios para hacer fácil la interacción con el usuario

--- ejercicio_provincias_listas.py
+++ ejercicio_provincias_listas.py
@@ -1,3 +1,8 @@
+"""	Pre:
+	Se crea un diccionario llamado "votos" que usara los nombres de..
+	los partidos como clave, y la cantidad de votos como valor
+	Pos: Devuelve diccionario con los partidos, y la cantidad de votos
+	"""
 def cargarVotos():
 	votos = {}
 	continuar = "s"
@@ -6,14 +11,19 @@ def cargarVotos():
 		votos[partido] = int(input("Ingresar cantida de votos del partido: "))
 		continuar = input("Desea continuar? Presione \"enter\" para salir... ")
 	return votos
-
+"""	Se recorre las n-provincias; por cada provincia, se pide cargar los votos"""
 def cargarVotosPorProvincia( cantidad_provincias ):
 	votos_por_provincia = []
 	for i in range(cantidad_provincias):
-		print("Provincia ", i)
+		print("Provincia ", i + 1)	#	i + 1 para que se muestre desde el 1
 		votos_por_provincia.append(cargarVotos())
 	return votos_por_provincia
 
+"""	Pre: votos_por_provincia tiene los votos por provincia
+	Se crea un diccionario de partidos, donde se acumulan los votos
+	por cada provincia. Si el partido no se encuentra en dicho diccionario,
+	se lo añadirá como si fuera una clave nueva; sino se acumula.
+	Pos: devuelve el diccionario de partidos, con la cantidad de votos totales"""
 def calcularVotosPorPartido( votos_por_provincia, cantidad_provincias ):
 	votos_por_partido = {}
 	for i in range(cantidad_provincias):
@@ -24,9 +34,14 @@ def calcularVotosPorPartido( votos_por_provincia, cantidad_provincias ):
 				votos_por_partido[partido] += votos_por_provincia[i][partido]
 	return votos_por_partido
 
+def mostrarVotosPorPartido( votos_por_partido ):
+	print("######Resultados de los votos#######")
+	for partido in votos_por_partido:
+		print(partido, ": ", votos_por_partido[partido])
+
 def main():
 	cantidad_provincias = 3
 	votos_por_provincia = cargarVotosPorProvincia(cantidad_provincias)
 	votos_por_partido = calcularVotosPorPartido( votos_por_provincia, cantidad_provincias)
-
+	mostrarVotosPorPartido( votos_por_partido )
 main()
